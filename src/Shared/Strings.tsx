2c33d3138ecd06357faef95af3a85a926612dea5
hackerbotsupreme <ALOKE.GEEK@gmail.com>
2023-10-28 13:29:36 +0530

    Update repository

--- src/Shared/Strings.tsx
+++ src/Shared/Strings.tsx
@@ -1,6 +1,6 @@
 export default{
-    ABOUT_HEADING:'Hi! My name is Rahul,\n I’m Full Stack Developer based in North Carolina',
-    ABOUT_DESC:"I am a passionate and dedicated UI/UX designer with a strong focus on creating exceptional user experiences. I thrive on transforming complex ideas into intuitive and visually appealing designs. With a keen eye for detail and a user-centered approach, I strive to deliver impactful solutions that engage and delight users. Let's collaborate to bring your digital products to life!",
+    ABOUT_HEADING:'Hi! My name is Aloke,\n I’m Full Stack Developer based in Calkutta/kolkata',
+    ABOUT_DESC:"I am a passionate and dedicated full stack  designer with a strong focus on creating exceptional user experiences. I thrive on transforming complex ideas into intuitive and visually appealing designs. With a keen eye for detail and a user-centered approach, I strive to deliver impactful solutions that engage and delight users. Let's collaborate to bring your digital products to life!",
 
     SERVICES:'Services',
     FRONTED:'FRONTED DEV',
@@ -10,5 +10,5 @@ export default{
     BACKEND_DESC:'Developing application backend and Server side development using various backend technology',
 
     ONLINE:'ONLINE TEACHING',
-    ONLINE_DESC:'Teaching programming and coding on various platform like Youtube, Udemy etc. Free Course to the student'
+    ONLINE_DESC:'Teaching programming and coding on platform like Youtube. Free Course to the student'
 }
