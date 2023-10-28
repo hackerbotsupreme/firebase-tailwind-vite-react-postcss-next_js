ef5fe1d1ff339bb920d577b45d231d786fb52fd0
hackerbotsupreme <ALOKE.GEEK@gmail.com>
2023-10-28 12:58:46 +0530

    Update repository

---
+++ src/Components/ProfilePic.tsx
@@ -0,0 +1,12 @@
+
+
+function ProfilePic() {
+  return (
+    <div className='fixed mt-[90px]'>
+        <img src="/user-image.jpg"
+        className='h-screen object-cover w-screen'/>
+    </div>
+  )
+}
+
+export default ProfilePic
